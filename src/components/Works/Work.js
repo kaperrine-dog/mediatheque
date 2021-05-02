2246aa023aff9e4fd522078ef90e5e27994f798e
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-02 19:26:46 +0900

    added styles and integrated a component of Work card

--- src/components/Works/Work.js
+++ src/components/Works/Work.js
@@ -10,9 +10,9 @@ const StyledWorkItem = styled.article`
   position: relative;
   min-width: 325px; 
   max-width: 500px;
-  border-radius: var(--blogCardBorderRadius);
+  border-radius: var(--itemCardBorderRadius);
   background: var(--background);
-  background: var(--blogPanelBGDark);
+  background: var(--itemPanelBGDark);
   box-shadow:  -20px 20px 40px var(--neumorphismShadow),
                 20px -20px 40px var(--neumorphizmLight);
   
@@ -23,16 +23,22 @@ const StyledWorkItem = styled.article`
   @media (min-width: 992px){
     max-width: 400px;
   }
-  @media (min-width: 1200px){
-    padding: 20px 20px;
+  @media (min-width: 1300px){
+    padding: 40px 35px;
   }
   @media (min-width: 1400px){
-    padding: 40px 20px;
+    padding: 40px 35px;
+  }
+  h2 {
+    margin: 0 0 20px;
+    padding: 0 0 0;
+    display: inline-block;
   }
   .workImage{
+    margin: 0 0 20px;
     width: 100%;
-    height: 250px;
-    border-radius: var(--blogCardBorderRadius);
+    height: 200px;
+    border-radius: var(--itemCardBorderRadius);
   }
   &:before{
     content: '';
@@ -41,7 +47,7 @@ const StyledWorkItem = styled.article`
     z-index: -1;
     width: 90%;
     height: 90%;
-    background-color: var(--blogCardShadowDark);
+    background-color: var(--itemCardShadowDark);
     filter: blur(20px);
     transform: translateY(10px) scale(1);
     mix-blend-mode: multiply;
@@ -51,7 +57,14 @@ const StyledWorkItem = styled.article`
 const StyledWorkContent = styled.div`
   //background: var(--worksPanelBG);
   //border-top: 3px solid var(--primary);
-  padding: 2.5rem 1.25rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  //padding: 2.5rem 1.25rem;
+  span{
+    //padding: 0 0 6px;
+  }
   &:last-child {
     margin-bottom: 0;
   }
@@ -63,30 +76,49 @@ const StyledWorkContent = styled.div`
   @media (min-width: 1000px) {
     max-width: calc((100vw - 60px - 80px) / 3);
   }
+`
 
-  h2 {
-    margin-top: 0;
-    display: inline-block;
-  }
+const StyledWorksIntro = styled.p`
+  margin: 0 0 20px;
+  width: 100%;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;  
+  overflow: hidden;
+`
 
-  p {
-    margin-bottom: 0;
-  }
+const StyledURL = styled.span`
+  
 `
 
 const Work = ({ work }) => {
-  const { name, slug, images, introduction } = work
+  const { name, slug, images, introduction, url, workId } = work
 
   const image = getImage(images[0])
 
   return (
     <StyledWorkItem>
+      <h2>{name || "Name not listed"}</h2>
       <GatsbyImage className="workImage" image={image} alt={introduction} />
       <StyledWorkContent>
-        <h2>{name || "Name not listed"}</h2>
         {/* <h3>£ { || "Call"}</h3> */}
-        <AniLink className="btn" cover bg="#1d1d1d" to={`/works/${slug}`}>
-          View Work
+        <StyledURL>
+          {url && url}
+        </StyledURL>
+        <StyledWorksIntro>{introduction && introduction}</StyledWorksIntro>
+        <AniLink 
+          //cover 
+          //paintDrip 
+          //hex="#ffffff99"
+          swipe
+          direction="up"
+          bg="var(--background)" 
+          entryOffset={80}
+          duration={0.75}
+          to={`/works/${slug}`} 
+          className="btn" >
+          View More
         </AniLink>
       </StyledWorkContent>
     </StyledWorkItem>
