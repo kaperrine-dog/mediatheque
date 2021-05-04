c43a901876482c97d9ead822da531e6984144bab
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-04 22:21:03 +0900

    style: create: integrate swiperslider component

--- src/components/SwiperSlider/SwiperSlider.js
+++ src/components/SwiperSlider/SwiperSlider.js
@@ -2,62 +2,45 @@ import {GatsbyImage} from "gatsby-plugin-image";
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
-import SwiperCore, {A11y, Autoplay, EffectFade, Navigation, Pagination, Scrollbar} from 'swiper';
-import 'swiper/components/effect-fade/effect-fade.min.css';
+import SwiperCore, {A11y, Autoplay, EffectCoverflow, EffectCube, EffectFade, EffectFlip, Navigation, Pagination, Scrollbar} from 'swiper';
+import 'swiper/components/navigation/navigation.min.css';
+//import 'swiper/components/effect-fade/effect-fade.min.css';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-/* 
-export const query = graphql `
-query($slug: String) {
-    work: contentfulWorks(slug: { eq: $slug }) {
-      name
-      introduction
-      description {
-        description
-      }
-      accordion {
-        title
-        description
-      }
-      images {
-        fluid {
-          ...GatsbyContentfulFluid_tracedSVG
-        }
-        gatsbyImageData
-        description
-        title
-        workImageId: contentful_id
-      }
-      url
-    }
-  }
-` */
 
 // install Swiper modules
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, EffectFade,EffectCoverflow, EffectFlip, EffectCube , Autoplay]);
 
-const SwiperSlider = ({images}) => {
+const SwiperSlider = ({ images }) => {
 
+  const [mainImage, ...workImages] = images
 	return (
-
 		<> {
-				images && images.length > 0 && ( 
+				workImages && workImages.length > 0 && ( 
 					<StyledSwiperSliderWrapper>
 					<Swiper 
-						effect="fade"
-						//loop="false"
-						spaceBetween = { 0 }
+						effect="coverflow"
+						//centeredSlides="true"
+						//loop="ture"
+						spaceBetween = { 100 }
 						slidesPerView = { 1 }
 						speed={ 1000 }
-						autoplay = { {
-								delay: 5000
-							} }
-						//navigation 
+						/* autoplay = { {
+							delay: 5000
+						} } */
+						navigation
+						navigation = {{
+							//prevEl: '.buttonPrev',
+              //nextEl: '.buttonNext',
+						}} 
 						pagination = { {
-								clickable: true
+								clickable: true,
+								//el: ".swiperPagination"
+								
 							} }
 						scrollbar = { {
-								draggable: true
+								draggable: true,
+								//el: ".swiper-scrollbar"
 							}
 						}
 						onSwiper = { (swiper) => {
@@ -71,14 +54,14 @@ const SwiperSlider = ({images}) => {
 						}
 					>
 					{
-						images && (
-							images.map((sliderImage, i) => {
+						workImages && (
+							workImages.map((sliderImage, i) => {
 								return ( 
 								<SwiperSlide 
 									className = "swiperSliderImageWrapper" 
 									key={'SwiperSlide' + sliderImage.workImageId + '-' + i}
 									>
-									<StyledImageOverlay></StyledImageOverlay>
+									{/* <StyledImageOverlay></StyledImageOverlay> */}
 									<StyledSwperSliderImage
 										key={sliderImage.workImageId + '-' + i}
                     >
@@ -86,7 +69,7 @@ const SwiperSlider = ({images}) => {
                       image={sliderImage.gatsbyImageData} 
                       className = 'swiperSliderImage'
                       alt = {
-                        sliderImage.title
+                        sliderImage.description
                       }
                     />
                   </StyledSwperSliderImage>
@@ -110,21 +93,110 @@ SwiperSlider.propTypes = {
 //StyledComponents
 
 const StyledSwiperSliderWrapper = styled.div`
+  margin: 40px 0 40px;
   position: relative;
-	color: var(--tertiary);
+	//color: var(--secondary);
+	.swiper-wrapper{
+		width: 100%;
+	}
+  .swiper-pagination-bullets{
+		display: flex;
+		justify-content:center;
+		margin: 0 0 10px;
+	}
+	.swiper-pagination-bullet{
+		box-sizing: content-box;
+		border: 2px solid rgba(0,0,0,0);
+	} 
+	.swiper-pagination-bullet-active{
+		background: rgba(0,0,0,0);
+		border: 2px solid var(--parallaxBG);
+		
+	}
+	.swiper-button-prev{
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 10px;
+		margin: auto 0;
+		background: var(--textColor);
+    border-radius: 50%;
+		display: flex;
+		justify-content:center;
+		align-items: center;
+		width: 25px;
+		height: 25px;		
+		@media(min-width: 769px){
+			width: 35px;
+			height: 35px;
+		}
+		&::after{
+			position: relative;
+			right: 1px;
+			font-weight: bold; 
+			color: var(--background);
+			font-size: 10px;
+			@media(min-width: 769px){
+				font-size: 14px;
+			}
+		}
+	}
+	.swiper-button-next{
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		right: 10px;
+		margin: auto 0;
+		background: var(--textColor);
+    border-radius: 50%;
+		width: 25px;
+		height: 25px;		
+		@media(min-width: 769px){
+			width: 35px;
+			height: 35px;
+		}
+		&::after{
+			position: relative;
+			left: 1px;
+			font-weight: bold;
+			color: var(--background);
+			font-size: 10px;
+			@media(min-width: 769px){
+				font-size: 14px;
+			}
+		}
+	}
 `
 
 const StyledSwperSliderImage = styled.div`
 	.swiperSliderImage{
-    width: 100%;
-    height: 70%;
-    object-fit: cover;
-    object-position: center;
-    //filter: hue-rotate(170deg);
-    //-webkit-filter: hue-rotate(170deg);
-    z-index: 1;
-		img{
+		margin: auto;
+		width: 90vw;
+		height: calc(90vw * 0.5625);
+		min-width: 300px;
+		min-height: 169px;
+		//aspect-ratio: 16 / 9;
+		position: relative;
+		overflow: hidden;
+		z-index: 1;
+		
+		@media (min-width: 769px){
 			width: 100%;
+			height: 500px;
+		}
+		//filter: hue-rotate(170deg);
+    //-webkit-filter: hue-rotate(170deg);
+		
+		img{
+			margin: auto;
+			width: 100%;
+			height: 100%;
+			//aspect-ratio: 16 / 9;
+			//height: 500px;
+			//max-height: 400px;
+			object-fit: cover;
+			object-position: center;
+			
 		}
   }
 `
