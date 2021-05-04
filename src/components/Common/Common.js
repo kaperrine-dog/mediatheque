617cd929815f4fc49ea4c79d04b874b7eeb83e24
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-04 22:18:43 +0900

    create common action

---
+++ src/components/Common/Common.js
@@ -0,0 +1,10 @@
+
+export const deleteAllCookies = () => {
+  const cookies = document.cookie.split(';')
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i]
+    const eqPos = cookie.indexOf('=')
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+    document.cookie = name + '=;max-age=0'
+  }
+}
