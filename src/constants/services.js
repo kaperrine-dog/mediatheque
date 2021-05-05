dddebdf13cbac66312ad67df4543ff06ed35fc2e
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-05 18:58:14 +0900

    add dummy sentences

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,17 +1,17 @@
 export default [
   {
-    title: "Super-fast loading times",
+    title: "タイトル01",
     text:
-      "Built using the React-based framework, Gatsby, you'll benefit from ultra-fast loading times and near instant page transitions.",
+      "親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）",
   },
   {
-    title: "Full Content Management",
+    title: "タイトル02",
     text:
-      "Using Contentful, you'll be able to update your site as your application grows. The days of using a bloated WordPress CMS are over.",
+      "タイトルが入ります。親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。",
   },
   {
-    title: "Modern & Clean Design",
+    title: "タイトル03",
     text:
-      "Helping your application stay up-to-date and attractive to users, Barcadia is made using minimalist-inspired design principles.",
+      "タイトルが入ります。親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。",
   },
 ]
