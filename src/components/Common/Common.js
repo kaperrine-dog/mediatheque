c02fee6572b3762e6f5ff6ea78c762e1336faef2
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-05 18:56:34 +0900

    added common used component

--- src/components/Common/Common.js
+++ src/components/Common/Common.js
@@ -1,3 +1,4 @@
+import {useCallback, useEffect, useState} from 'react';
 
 export const deleteAllCookies = () => {
   const cookies = document.cookie.split(';')
@@ -8,3 +9,23 @@ export const deleteAllCookies = () => {
     document.cookie = name + '=;max-age=0'
   }
 }
+
+export const useParentElementDimensions = () => {
+  const isClient = typeof window === 'object';
+  const getWindowDimensions = useCallback(() => {
+    return {
+      width: isClient ? window?.parentElement.clientWidth : 0,
+      height: isClient ? window?.parentElement.clientHeight : 0
+    };
+  }, [isClient]);
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  useEffect(() => {
+    const onResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, [getWindowDimensions]);
+  return windowDimensions;
+};
+
