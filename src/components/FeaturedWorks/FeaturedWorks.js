ffff1e81d06030f8449b8e60f9e354039a2e916e
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-05 19:14:24 +0900

    style: margin bottom of p 0 to 2em

--- src/components/FeaturedWorks/FeaturedWorks.js
+++ src/components/FeaturedWorks/FeaturedWorks.js
@@ -21,10 +21,7 @@ const getWorks = graphql`
             description
           }
           images {
-            gatsbyImageData(
-              width: 600
-              formats: [AUTO, WEBP]
-              )
+            gatsbyImageData(width: 600, formats: [AUTO, WEBP])
           }
         }
       }
@@ -34,6 +31,9 @@ const getWorks = graphql`
 const TitleArea = styled.div`
   @media (min-width: 769px) {
     grid-column: 1 / 2;
+  }
+  p{
+    margin: 0 0 2em;
   }
 `
 
@@ -81,9 +81,9 @@ const FeaturedWorks = ({ largePadding, id }) => {
     >
       <Grid>
         <TitleArea>
-          <Title>Displaying your featured works is easy</Title>
+          <Title>Featured works</Title>
           <p>
-            
+          親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）
           </p>
           <Button text="View All Works" link="/works" />
         </TitleArea>
