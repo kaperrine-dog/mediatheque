c6669e5436c66b937919c757b1ae0d239f0b7b9a
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-09 14:37:16 +0900

    added social icons

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -1,10 +1,19 @@
+import githubIconDark from "../images/icons/githubDark.svg"
+import githubIconLight from "../images/icons/githubLight.svg"
+import qiitaIconDark from "../images/icons/qiitaDark.png"
+import qiitaIconLight from "../images/icons/qiitaLight.png"
+
 export default [
   {
     title: "Qiita",
     url: "https://qiita.com/wang_chang",
+    iconLight: qiitaIconLight,
+    iconDark: qiitaIconDark,
   },
   {
     title: "GitHub",
     url: "https://github.com/kaperrine-dog",
+    iconLight: githubIconLight,
+    iconDark: githubIconDark,
   },
 ]
