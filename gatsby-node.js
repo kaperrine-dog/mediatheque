e3122b33415d7d72ca372b35bcc8af9b3542db9b
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-12 23:48:16 +0900

    removed error handling on node_modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -69,3 +69,18 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+/* exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-hook-recaptcha/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+} */
