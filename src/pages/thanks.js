1e8d30b8f4e5dc0f3496c3dadcb7396ee3ed5f87
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-15 22:25:41 +0900

    create thanks page

---
+++ src/pages/thanks.js
@@ -0,0 +1,107 @@
+import {Link} from "gatsby"
+import React from "react"
+import styled from "styled-components"
+import Grid from "../components/Grid/Grid"
+import Network from "../components/Network/Network.js"
+import Seo from "../components/SEO"
+
+const Thanks = styled.section`
+  background-color: var(--background);
+  color: var(--text-color);
+
+`
+
+const Title = styled.h1`
+  margin-top: 0;
+
+  @media (min-width: 769px) {
+    grid-column: 1 / 2;
+  }
+`
+
+const SubContent = styled.div`
+
+  p {
+    margin-bottom: 2.125rem;
+  }
+  @media (min-width: 769px) {
+    grid-column: 2 / 4;
+  }
+`
+
+const SubTitle = styled.h2`
+  @media (min-width: 769px) {
+    margin-top: 0;
+  }
+`
+const NetWorkRight = styled.div`
+  width: 100%;
+  height: 100%;
+  //display: none;
+  @media (min-width: 769px) {
+    display: initial;
+    grid-column: 3 / 4;
+
+  }
+`
+const NetWorkCenter = styled.div`
+  width: 100%;
+  height: 100%;
+  @media (min-width: 769px) {
+    display: initial;
+    grid-column: 2 / 3;
+  }
+`
+const NetWorkLeft = styled.div`
+  width: 100%;
+  height: 100%;
+  //display: none;
+  @media (min-width: 769px) {
+    display: initial;
+    grid-column: 1 / 2;
+  }
+`
+
+const thanks= () => {
+  return (
+    <>
+      <Seo title="Thanks!" />
+      <Thanks className="section-padding">
+        <Grid>
+          <Title>Thanks</Title>
+          <SubContent>
+            <SubTitle>お問い合わせありがとうございます。</SubTitle>
+            <p>
+            </p>
+          </SubContent>
+          <NetWorkLeft>
+            <Network
+              id = "canvasLeft"
+              randomRangeMin = {-10}
+              randomRangeMax = {24}
+            />
+          </NetWorkLeft>
+          <NetWorkCenter>
+             <Network
+              id = "canvasCenter"
+              randomRangeMin = {-40}
+              randomRangeMax = {14}
+            /> 
+          </NetWorkCenter>
+          <NetWorkRight>
+            <Network
+              id = "canvasRight"
+              randomRangeMin = {-10}
+              randomRangeMax = {10}
+            />
+          </NetWorkRight>
+            <Link className="btn" to="/">
+              Homeに戻る
+            </Link>
+        </Grid>
+      </Thanks>
+    </>
+  )
+}
+
+export default thanks
