b1be885a538b98280c174363cbd08f05516326d8
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-17 00:36:36 +0900

    added self introduction

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,8 @@ import React from "react"
 import {Link} from "react-scroll"
 import styled from "styled-components"
 import Grid from "../Grid/Grid"
+import SelfIntroduction from "../SelfIntroduction/SelfIntroduction.js"
+
 
 const TitleArea = styled.div`
   grid-column: 1 / 4;
@@ -45,10 +47,7 @@ const About = ({ largePadding, id }) => {
           </Link>
         </TitleArea>
         <ContentArea>
-          <p>
-            いい感じの文章ここに配置 <br/>
-            親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）
-          </p>
+          <SelfIntroduction/>
         </ContentArea>
       </Grid>
     </AboutSection>
