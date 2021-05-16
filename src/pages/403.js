a873a9b9cde96d2054f482fb111ff0c4723eae7f
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-17 00:35:13 +0900

    added 403 redirect page

---
+++ src/pages/403.js
@@ -0,0 +1,62 @@
+import React, {useEffect} from "react"
+import styled from "styled-components"
+import Grid from "../components/Grid/Grid"
+import Seo from "../components/SEO"
+
+const Error404 = styled.section`
+  background-color: var(--background);
+  color: var(--text-color);
+`
+
+const Title = styled.h1`
+  margin-top: 0;
+
+  @media (min-width: 769px) {
+    grid-column: 1 / 2;
+  }
+`
+
+const SubContent = styled.div`
+  p {
+    margin-bottom: 2.125rem;
+  }
+  @media (min-width: 769px) {
+    grid-column: 2 / 3;
+  }
+`
+
+const SubTitle = styled.h2`
+  @media (min-width: 769px) {
+    margin-top: 0;
+  }
+`
+
+const Error = () => {
+  const redirect = () => {
+    if( typeof window !== `undefined` ){
+      window.location.href = GATSBY_403_REDIRECT_URL
+    }
+  }
+  useEffect(
+  redirect()
+  )
+  return (
+    <>
+      <Seo title="Error" />
+      <Error404 className="section-padding">
+        <Grid>
+          <Title>403 FORBIDDEN</Title>
+          <SubContent>
+            <SubTitle>アクセスが禁止されています。</SubTitle>
+            <p>
+              You're connecting blocked IP adress.<br/>
+              Redirecting...
+            </p>
+          </SubContent>
+        </Grid>
+      </Error404>
+    </>
+  )
+}
+
+export default Error
