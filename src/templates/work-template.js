a5411a4156d660d53fd03dbcd6203bbefd4c71d6
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-29 12:49:28 +0900

    modified header styles

--- src/templates/work-template.js
+++ src/templates/work-template.js
@@ -1,8 +1,8 @@
-import { graphql } from "gatsby";
+import {graphql} from "gatsby";
 import React from "react";
 import styled from "styled-components";
 import Accordion from "/src/components/Accordion/Accordion";
-import { deleteAllCookies } from '/src/components/Common/Common.js';
+import {deleteAllCookies} from '/src/components/Common/Common.js';
 import Grid from "/src/components/Grid/Grid";
 import Seo from "/src/components/SEO";
 import SwiperSlider from "/src/components/SwiperSlider/SwiperSlider.js";
@@ -19,7 +19,7 @@ const StyledNarrowImageArea = styled.div`
   }
   @media (min-width: 769px){
     max-width: 350px;
-    margin: 95px auto 0;
+    margin: 0 auto 0;
     display: initial;
     width: 100%;
     grid-column: 2 / 3;
@@ -31,7 +31,7 @@ const StyledNarrowImageArea = styled.div`
   }
   @media (min-width: 1000px) {
     max-width: 375px;
-    margin: 95px 0 0 auto;
+    margin: 0 0 0 auto;
     display: initial;
     width: 100%;
     grid-column: 3 / 4;
@@ -60,6 +60,26 @@ const ContentArea = styled.div`
   }
   &:last-child{
     
+  }
+`
+
+const StyledHeader = styled.div`
+  grid-column: 1 / 1;
+  width: 100%;
+  @media (min-width: 769px){
+    width: 100%;
+    grid-column: 1 / 1;
+  }
+  @media (min-width: 1000px) {
+    width: 100%;
+    grid-column: 1 / 4;
+  }
+  h1 {
+    margin-top: 0;
+    text-transform: capitalize;
+  }
+  p {
+    margin-bottom: 40px;
   }
 `
 
@@ -96,10 +116,27 @@ const workTemplate = ({ data }) => {
   const [ bannerImage, ...mainImages ] = images
   const [ ...subImages ] = narrowImages
   const contentHtml = description.childMarkdownRemark.html
-  //console.log( description.childMarkdownRemark.html )
   return <>
     <Seo title={name} />
     <section className="section-padding">
+      <Grid>
+        <StyledHeader>
+          <h1>{ name }</h1>
+            { url &&
+            <StyledURL
+              onClick ={ linkToOtherSites }
+              className = "btnEmbed"
+              >
+                {  `https://www.${url}/` }
+            </StyledURL>
+            }
+            { introduction && 
+            <StyledIntroduction>
+              {introduction}
+            </StyledIntroduction>
+            }
+        </StyledHeader>
+      </Grid>
       <Grid>
         { subImages && 
         <StyledNarrowImageArea
@@ -107,7 +144,7 @@ const workTemplate = ({ data }) => {
         >
           <Accordion
             title = {"スマートフォン画面キャプチャ"}
-            state = {true}
+            state = {false}
           >
           <SwiperSlider
             images = { subImages }
@@ -116,20 +153,7 @@ const workTemplate = ({ data }) => {
         </StyledNarrowImageArea>
           }
         <ContentArea>
-          <h1>{ name }</h1>
-          { url &&
-          <StyledURL
-            onClick ={ linkToOtherSites }
-            className = "btnEmbed"
-            >
-              {  `https://www.${url}/` }
-          </StyledURL>
-          }
-          { introduction && 
-          <StyledIntroduction>
-            {introduction}
-          </StyledIntroduction>
-          }
+          
           <article
             className="contentArticle"
             dangerouslySetInnerHTML={{ __html: contentHtml }}
@@ -143,7 +167,7 @@ const workTemplate = ({ data }) => {
           <StyledImageGallery>
             <Accordion
               title = {'PC画面キャプチャ'}
-              state = {true}
+              state = {false}
             >
               <SwiperSlider
                 images = { mainImages }
