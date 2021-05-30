d567af9f46c7501cf3035a3c8f7dff321a95bdc7
Kaperrine-dog <61542937+kaperrine-dog@users.noreply.github.com>
2021-05-30 16:38:18 +0900

    modified using Functional Component instead of React Component

--- src/components/Works/WorkList.js
+++ src/components/Works/WorkList.js
@@ -1,61 +1,24 @@
-import React, {Component} from "react"
-import styled from "styled-components"
+import React from "react"
+import Pagination from "../Pagination/Pagination.js"
 import Work from "./Work"
 
-const StyledSection = styled.section`
-  margin: auto;
-  width: calc(100vw - var(--paddingBorder) * 2);
-  @media (min-width: 769px){
-    width: 100%;
-    grid-column: 1 / 4;
-    margin-left: -20px;
-    margin-right: -20px;
-  }
-
-`
-
-const StyledFlexContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`
-
-const StyledFlexItem = styled.div`
-  width: 100%;
-  margin-bottom: 40px;
-  &:last-child {
-    margin-bottom: 0;
-  }
-  @media (min-width: 769px) {
-    flex: 0 0 calc(100% / 2);
-  }
-  @media (min-width: 1000px) {
-    flex: 0 0 calc(100% / 3);
-  }
-`
-
-export default class WorkList extends Component {
-  state = {
-    works: [],
-    sortedWorks: [],
-  }
-
-  componentDidMount() {
-    this.setState({
-      works: this.props.works.edges,
-      sortedWorks: this.props.works.edges,
-    })
-  }
-  render() {
-    return (
-        <>
-          {this.state.sortedWorks.map(({ node }, index) => {
-            return (
-                <Work 
-                  key={node.workId /* node.contentful_id */} 
-                  work={node} />
-                  )
-            })}
-        </>
-    )
-  }
+const WorkList = ( {works, pageContext, basePath}) => {
+  
+  return (
+    <>
+      {works.edges.map(({ node }, index) => {
+        return (
+            <Work 
+              key={ node.workId } 
+              work={node} />
+              )
+        })}
+      <Pagination
+        pageContext = {pageContext}
+        basePath = {basePath}
+      />
+    </>
+  )
 }
+
+export default WorkList
